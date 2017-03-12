4d207157d8da0dd327d9af8ea1ba0bcf0f4dafc2
jasonleor <305859613@qq.com>
2017-03-12 14:16:29 +0800

    0312 整合 mybatis

--- src/main/java/com/jason/controller/IndexController.java
+++ src/main/java/com/jason/controller/IndexController.java
@@ -1,14 +1,17 @@
 package com.jason.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Jason on 2017/3/9
  */
-@RestController
+@Controller
 public class IndexController {
     @RequestMapping("/")
+    @ResponseBody
     public String index() {
         return "hei";
     }
