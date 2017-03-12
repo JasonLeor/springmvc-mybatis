d05dcde76892637fb3326d07de6950dc60018c1f
jasonleor <305859613@qq.com>
2017-03-12 15:26:51 +0800

    整合 druid 连接池 莫名其妙 读不到properties的数据

--- src/main/java/com/jason/controller/IndexController.java
+++ src/main/java/com/jason/controller/IndexController.java
@@ -1,5 +1,8 @@
 package com.jason.controller;
 
+import com.jason.dao.HmUserMapper;
+import com.jason.model.HmUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,9 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Controller
 public class IndexController {
+    @Autowired
+    private HmUserMapper hmUserMapper;
     @RequestMapping("/")
     @ResponseBody
     public String index() {
+        HmUser user = hmUserMapper.selectByPrimaryKey(1);
+        System.out.print(user);
         return "hei";
     }
 }
